aabdec4a4d5afdac902201d9b51248b466625a68
Diego Vargas <91356068+dzarkV@users.noreply.github.com>
2024-02-19 06:59:58 +0000

    ModelSerializer unified in serializers

--- emailApp/serializers/message_from_serializer.py
+++ emailApp/serializers/message_from_serializer.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from emailApp.models import MessageFrom
 
 
-class MessageFromSerializer(serializers.HyperlinkedModelSerializer):
+class MessageFromSerializer(serializers.ModelSerializer):
     class Meta:
         model = MessageFrom
         fields = ('id', 'subject', 'body', 'created_at', 'from_user')
--- emailApp/serializers/user_serializer.py
+++ emailApp/serializers/user_serializer.py
@@ -6,5 +6,4 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
         fields = ('email', 'password', 'name')
-        read_only_fields = ('email',)
         

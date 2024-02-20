2ca2f8b89076ee6eb2f5040e1328994214484098
Julian <julian.ramirez04@uptc.edu.co>
2024-02-20 16:46:09 -0500

    implement view messages

--- emailApp/models/message_to.py
+++ emailApp/models/message_to.py
@@ -1,12 +1,15 @@
 from django.db import models
 from .user import User
-
+from .message_from import MessageFrom
 
 class MessageTo(models.Model):
     """
     MessageTo model to store the message to the user.
     """
 
-    message_id = models.AutoField(primary_key=True)
+    id = models.AutoField(primary_key=True)
+
+    message_id = models.ForeignKey(MessageFrom, on_delete=models.CASCADE)
 
     to_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    

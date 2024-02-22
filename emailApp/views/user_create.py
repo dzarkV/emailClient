cc0c9bf982d13a851c890b1f81ee2be0cd64c35a
dzarkV <dfvargasp@unal.edu.co>
2024-02-22 10:10:44 -0500

    users class cleaned

--- emailApp/views/user_create.py
+++ emailApp/views/user_create.py
@@ -20,7 +20,6 @@ class UserCreateView(generics.CreateAPIView):
             if 'email' not in request.data:
                 return Response({'email': ['This field is required']}, status=status.HTTP_400_BAD_REQUEST)
             
-            # Serialize the request data
             # Extract the password from the request
             password = request.data.get('password')
 
@@ -30,6 +29,7 @@ class UserCreateView(generics.CreateAPIView):
             # Replace the plain password with the hashed one in the request data
             request.data['password'] = hashed_password.decode('utf-8')
 
+            # Serialize the request data
             serializer = UserSerializer(data=request.data) 
             queryset = get_user_model().objects.all()
 
@@ -46,25 +46,4 @@ class UserCreateView(generics.CreateAPIView):
         except Exception as e:
             return Response({'message': 'Uncontrolled error: ' + str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-# class UserCreateView(generics.CreateAPIView):
-#     """
-#     Create a new user.
-#     """
-#     serializer_class = UserSerializer
-
-#     def post(self, request):
-#         """
-#         Create a new user. If the email already exists, return a 400 status code.
-#         """
-#         try:
-#             serializer = UserSerializer(data=request.data)
-#             queryset = get_user_model().objects.all()
-#             if queryset.filter(email=request.data['email']).exists():
-#                 return Response({'message': 'Email already exists'}, status=status.HTTP_400_BAD_REQUEST)
-#             if serializer.is_valid():
-#                 serializer.save()
-#                 return Response({'message': 'User created succesfully'}, status=status.HTTP_201_CREATED)
-#             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-#         except Exception as e:
-#             return Response({'message': 'Uncontrolled error:' + str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
